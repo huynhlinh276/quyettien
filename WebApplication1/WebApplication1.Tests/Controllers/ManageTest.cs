296959bfe7fcfd184ce9cbf3180f24abff946f2b
MyLinh <huynhlinh270698@gmail.com>
2018-12-05 09:39:38 +0700

    edit

---
+++ WebApplication1/WebApplication1.Tests/Controllers/ManageTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Collections.Generic;
+
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class ManageTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new ManageController();
+            var result = controller.Index() as ViewResult;
+           
+            var db = new cs4peEntities();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<BangSanPham>));
+          
+            Assert.AreEqual(db.BangSanPhams.Count(), ((List<BangSanPham>)result.Model).Count);
+        }
+    }
+}
